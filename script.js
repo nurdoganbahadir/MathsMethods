979c4e31624a99a4a5263b974548c8e802939357
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-14 18:37:26 +0300

    .round() added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let number = 128.75;
 
-//! .toFixed() sayının noktadan sonrasını atar ve büyük olan kısma sayıyı yuvarlar.
-console.log(number.toFixed());
+//! .toFixed() sayının noktadan sonrasını atar ve büyük olan kısma sayıyı yuvarlar. İçine değer alırsa noktadan sonrası kadar uzatır
+console.log(number.toFixed(1));
+
+//! .round() sayıyı normal şekilde yakın kısma yuvalar
+console.log(Math.round(number));
 
